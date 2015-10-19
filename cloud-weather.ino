c07bb35c23a1c180843a62f06ce1e7ed374d2e1d
AayushB <aayush.bhandarii@gmail.com>
2015-10-18 22:12:35 -0700

    rever adding json libs to compile.sh

--- cloud-weather.ino
+++ cloud-weather.ino
@@ -1,28 +1,25 @@
 // This #include statement was automatically added by the Particle IDE.
 // #include "Weather.h"
 // Weather weather("this is the api key", "94100");
-// #include "rest_client.h"
-// int statusCode;
-// String response;  
-// RestClient client = RestClient("query.yahooapis.com");
+#include "rest_client.h"
+int statusCode;
+String response;  
+RestClient client = RestClient("query.yahooapis.com");
 
 
 void setup() 
 {
   Serial.begin(9600);
-  // pinMode(D0,INPUT);
+  pinMode(D0,INPUT);
 }
 
 void loop() 
 {
-  // if(digitalRead(D0))
-  // {
-  // int statusCode = client.get("/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%2012797178&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",&response);
-  // Serial.println(response);
-  // unsigned first = response.find("text\":\"");
-  // unsigned last = response.find("\"}");
-  // String strNew = responsetr.substr (first,last-first);
-  // response="";
-  // delay(1000);
-  // }
+  if(digitalRead(D0))
+  {
+  int statusCode = client.get("/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%2012797178&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",&response);
+  Serial.println(response);
+  response="";
+  delay(1000);
+  }
 }
--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-particle compile photon cloud-weather.ino Weather/Weather.h Weather/Weather.cpp RestClient/rest_client.h RestClient/rest_client.cpp Json/jsmnSpark.h Json.jsmnSpark.cpp --saveTo firmware.bin
+particle compile photon cloud-weather.ino Weather/Weather.h Weather/Weather.cpp RestClient/rest_client.h RestClient/rest_client.cpp --saveTo firmware.bin
